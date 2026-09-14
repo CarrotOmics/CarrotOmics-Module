455596a69f0affbb6268d61bbb648df6e34ce172
Douglas Senalik <dsenalik@wisc.edu>
2026-09-14 17:54:59 -0500

    validate for duplicate entities published

--- src/Form/CarrotOmicsAdminValidate.php
+++ src/Form/CarrotOmicsAdminValidate.php
@@ -3,6 +3,8 @@
 namespace Drupal\carrotomics\Form;
 
 use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Entity\EntityFieldManager;
+use Drupal\Core\Entity\EntityTypeBundleInfo;
 use Drupal\Core\File\FileSystem;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\pgsql\Driver\Database\pgsql\Connection;
@@ -13,9 +15,23 @@ use Drupal\tripal\TripalBackendPublish\PluginManager\TripalBackendPublishManager
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides the user interface for CarrotOmics publication tools.
+ * Provides the user interface for CarrotOmics validation tools.
  */
 class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
+
+  /**
+   * Entity field manager service.
+   *
+   * @var Drupal\Core\Entity\EntityFieldManager
+   */
+  protected EntityFieldManager $entity_field_manager;
+
+  /**
+   * Bundle info service.
+   *
+   * @var Drupal\Core\Entity\EntityTypeBundleInfo
+   */
+  protected EntityTypeBundleInfo $bundle_info;
 
   /**
    * Drupal configuration service.
@@ -25,7 +41,7 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
   protected FileSystem $file_system;
 
   /**
-   * CarrotOmicsAdminPub class constructor.
+   * CarrotOmicsAdminValidate class constructor.
    *
    * Prepares injected services.
    */
@@ -36,9 +52,13 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
     TripalEntityLookup $entity_lookup_manager,
     TripalLogger $logger,
     TripalBackendPublishManager $publish_manager,
+    EntityFieldManager $entity_field_manager,
+    EntityTypeBundleInfo $bundle_info,
     FileSystem $file_system,
   ) {
     parent::__construct($config_factory, $drupal_connection, $chado_connection, $entity_lookup_manager, $logger, $publish_manager);
+    $this->entity_field_manager = $entity_field_manager;
+    $this->bundle_info = $bundle_info;
     $this->file_system = $file_system;
   }
 
@@ -53,6 +73,8 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
       $container->get('tripal.tripal_entity.lookup'),
       $container->get('tripal.logger'),
       $container->get('tripal.backend_publish'),
+      $container->get('entity_field.manager'),
+      $container->get('entity_type.bundle.info'),
       $container->get('file_system'),
     );
   }
@@ -81,7 +103,7 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
     $storage = $form_state->getStorage();
     if (isset($storage['results'])) {
       $form['pub_results'] = [
-        '#type'   => 'fieldset',
+        '#type' => 'fieldset',
         '#title' => 'Results',
         '#markup' => '<div class="form-results">' . $storage['results'] . '</div>',
         '#weight' => -100,
@@ -91,16 +113,35 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
     // ;;;
     if (FALSE) {
       // Add a 'Find Unpublished Content' button.
-      $form['find_unpub_btn'] = [
+      $form['val_find_unpub_btn'] = [
         '#type'   => 'submit',
-        '#name'   => 'find_unpub_btn',
+        '#name'   => 'val_find_unpub_btn',
         '#value'  => 'Find Unpublished Content',
         '#prefix' => '<div style="padding-top:30px;"><em>'
         . "List any Tripal content types that have unpublished content"
         . "</em></div><br />",
         '#suffix' => "<hr>",
       ];
+    } //;;;
 
+    // Add a 'Find double-published records' button and bundle select.
+    $form['val_bundle_id'] = [
+      '#type'   => 'select',
+      '#name' => 'val_bundle_id',
+      '#options' => $this->getBundleNames(),
+      '#prefix' => '<div style="padding-top:30px;"><em>'
+      . 'Finds chado records that have been published as more than one entity.'
+      . '</em></div><br />',
+    ];
+
+    $form['val_find_doublepublished_btn'] = [
+      '#type' => 'submit',
+      '#name' => 'val_find_doublepublished_btn',
+      '#value' => 'Find double-published records',
+      '#suffix' => '<hr>',
+    ];
+
+    if (FALSE) {
       // Add a 'Validate CarrotOmics accessions' button.
       $form['val_car_acc_btn'] = [
         '#type'   => 'submit',
@@ -121,11 +162,11 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
       '#name'   => 'val_fileloc_btn',
       '#value'  => 'Validate public:// chado.fileloc entries',
       '#prefix' => '<div style="padding-top:30px;"><em>'
-      . "Validate all files stored locally using the public:// URL"
-      . " nomenclature that are listed in the chado.fileloc table."
-      . " File presence, read permission, size, and MD5 checksum are validated."
-      . "</em></div><br />",
-      '#suffix' => "<hr>",
+      . 'Validate all files stored locally using the public:// URL'
+      . ' nomenclature that are listed in the chado.fileloc table.'
+      . ' File presence, read permission, size, and MD5 checksum are validated.'
+      . '</em></div><br />',
+      '#suffix' => '<hr>',
     ];
 
     // ;;;
@@ -146,6 +187,23 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
   }
 
   /**
+   * Gets a list of bundle names.
+   *
+   * @return array
+   *   The list of bundle names.
+   */
+  protected function getBundleNames(): array {
+    $entity_type = 'tripal_entity';
+    $bundles = $this->bundle_info->getBundleInfo($entity_type);
+    $select_list = ['' => '--Select--'];
+    // $select_list = ['all' => '--All Bundles--'];
+    foreach ($bundles as $bundle_id => $info) {
+      $select_list[$bundle_id] = $info['label'];
+    }
+    return $select_list;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
@@ -155,11 +213,17 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
     // Gets the triggering element, i.e. which button was pressed.
     $triggering_element = $form_state->getTriggeringElement()['#name'];
 
+    // Gets the values from form input fields.
+    $bundle_id = $form_state->getValue('val_bundle_id');
+
     // Take action depending on which button was pressed.
     $nerrors = 0;
     $status = '';
-    if ($triggering_element == 'find_unpub_btn') {
+    if ($triggering_element == 'val_find_unpub_btn') {
       [$nerrors, $status] = carrotomics_admin_find_unpublished();
+    }
+    elseif ($triggering_element == 'val_find_doublepublished_btn') {
+      [$nerrors, $status] = $this->findDoublepublished($bundle_id);
     }
     elseif ($triggering_element == 'val_car_acc_btn') {
       [$nerrors, $status] = carrotomics_admin_validate_carrotomics_accessions();
@@ -177,6 +241,74 @@ class CarrotOmicsAdminValidate extends CarrotOmicsAdminFormBase {
     if ($nerrors) {
       $this->messenger()->addError($nerrors . ' errors');
     }
+  }
+
+  /**
+   * Find and unpublished chado records published more than once.
+   *
+   * This probably happened because of some bug.
+   *
+   * @param string $bundle_id
+   *   The name of the bundle to check.
+   */
+  protected function findDoublepublished(string $bundle_id) {
+    if (!$bundle_id) {
+      $this->messenger()->addError("Bundle not selected");
+      return;
+    }
+
+    // Get a required field for this bundle, plus the base table.
+    $base_table = NULL;
+    $required_field = NULL;
+    $fields = $this->entity_field_manager->getFieldDefinitions('tripal_entity', $bundle_id);
+    foreach ($fields as $field_name => $field_info) {
+      $storage_settings = $field_info->getSetting('storage_plugin_settings');
+      $base_table = $storage_settings['base_table'] ?? '';
+      $base_column = $storage_settings['base_table_dependant']['base_column']
+        ?? $storage_settings['base_column'] ?? '';
+      $is_required = $field_info->isRequired();
+      if ($is_required and $base_table and $base_column) {
+        $required_field = $field_name;
+        break;
+      }
+    }
+    if (!$required_field) {
+      $this->messenger()->addError("No required fields for bundle \"$bundle_id\"");
+      return;
+    }
+
+    // Set up a drupal field table query.
+    $table_name = 'tripal_entity__' . $required_field;
+    $record_id_column = $required_field . '_record_id';
+    $query = $this->drupal_connection->select($table_name, 'T');
+    $query->addField('T', 'entity_id', 'entity_id');
+    $query->addField('T', $record_id_column, 'record_id');
+    $query->condition('T.bundle', $bundle_id, '=');
+    $query->orderBy('entity_id');
+    $results = $query->execute();
+
+    // Parse query results.
+    $items = [];
+    while ($result = $results->fetchObject()) {
+      $items[$result->record_id][] = $result->entity_id;
+    }
+
+    // Count duplicates.
+    $n_duplicates = 0;
+    $n_summarized = 0;
+    $first_several = '';
+    foreach ($items as $record_id => $values) {
+      if (count($values) > 1) {
+        $n_duplicates++;
+        if ($n_duplicates <= 10) {
+          $n_summarized++;
+          $first_several .= '<br>record_id ' . $record_id . ': entities(' . implode(', ', $values) . ')';
+        }
+      }
+    }
+
+    $subset_label = ($n_summarized == $n_duplicates) ? '' : ' First ' . $n_summarized;
+    return [$n_duplicates, $n_duplicates . ' duplicates found' . $subset_label . ': ' . $first_several];
   }
 
   /**
